c32a12b130d93db5b78306cbe7fe848d19755682
Raihanbs12 <raihanbs12.24@gmail.com>
2025-12-08 13:38:50 +0700

    koneksi

---
+++ koneksi.php
@@ -0,0 +1,19 @@
+<?php
+
+$host     = "localhost";   // biasanya tetap localhost kalau pakai hosting
+$user     = "root";        // username database (kalau di hosting biasanya bukan root)
+$password = "";            // isi password MySQL kamu
+$database = "eskristal_warid"; // nama database yang kamu buat
+
+// Koneksi ke MySQL
+$conn = mysqli_connect($host, $user, $password, $database);
+
+// Cek koneksi
+if (!$conn) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}
+
+// Optional (untuk memastikan format karakter aman, terutama input form)
+mysqli_set_charset($conn, "utf8");
+
+?>
